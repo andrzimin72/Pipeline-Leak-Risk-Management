afeb1f0ccd39324b7f237f0b568a3cf7c3c3cdb2
Andre Zimin <130975402+andrzimin72@users.noreply.github.com>
2026-09-19 18:30:43 +0800

    Add files via upload

--- edge/sensors/__init__.py
+++ edge/sensors/__init__.py
@@ -1,1 +1,5 @@
-
+"""
+Physical Sensor Listeners.
+MQTT publishers for the 10 physical sensors (Acoustic, Pressure, Coriolis, 
+Fiber DTS, Temperature, Soil Moisture, Capacitance, Vibration, Corrosion, Flow).
+"""
